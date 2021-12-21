c4f38ad4f616817e489b61aa21d563e2af99598e
Joe Tom <jtom@sonatype.com>
2021-12-21 18:48:19 -0500

    checking for more comments

--- github.js
+++ github.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { omponent} from 'react';
+import {connect} from 'react-redux';
 
 class Post extends Component {
   render() {
