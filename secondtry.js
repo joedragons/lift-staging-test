fc0a978862f35dbc6dd6ac8ac9ac52f5ad139074
Joe Tom <jtom@sonatype.com>
2022-01-06 18:00:36 -0500

    LIFT-2187
    take 2, testing vs bitbucket this time with local bot

---
+++ secondtry.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Post extends Component {
+  render() {
+    return (
+      <div>
+        {console.log('test')}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    posts: state
+  }
+};
+
+export default Post;
